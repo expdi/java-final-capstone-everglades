edf212be8bde8bb17409515a60af8eabfb6459ce
alan aguillon <alan.aguillonjuarez@expeditors.com>
2024-06-05 09:56:41 -0500

    test faltantes

--- pricingservice/src/test/java/com/expeditors/pricingservice/controller/PricingControllerIntegrationTest.java
+++ pricingservice/src/test/java/com/expeditors/pricingservice/controller/PricingControllerIntegrationTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -16,19 +17,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 public class PricingControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     @Test
+    @WithMockUser(roles = "ADMIN")
     void getPrice_ReturnsOKAndPrice() throws Exception {
         mockMvc.perform(MockMvcRequestBuilders.get("/pricing"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
     @Test
+    @WithMockUser(roles = "ADMIN")
     public void testSetLimitsWithGoodInputGivesNoContent() throws Exception {
         var actions = mockMvc.perform(put("/pricing/{lowerlimit}/{upperLimit}", 15.0, 60.0))
                 .andExpect(status().isNoContent()).andReturn();
@@ -43,6 +46,7 @@ public class PricingControllerIntegrationTest {
         assertEquals("Lower limit: 15.0, Upper Limit: 60.0", bothLimits);
     }
     @Test
+    @WithMockUser(roles = "ADMIN")
     public void testSetLimitsWithBadInputGivesBadRequest() throws Exception {
         var actions = mockMvc.perform(put("/pricing/{lowerLimit}/{upperLimit}", 65.0, 60.0))
                 .andExpect(status().isBadRequest()).andReturn();
