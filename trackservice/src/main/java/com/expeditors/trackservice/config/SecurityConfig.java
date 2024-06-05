79c34ba2ea7d17c9a86537f9a310d81f59f98178
Antony Alfaro <antony.alfaro@expeditors.com>
2024-06-05 12:17:05 -0600

    remove application.properties file and add missing anotation to security config file

--- trackservice/src/main/java/com/expeditors/trackservice/config/SecurityConfig.java
+++ trackservice/src/main/java/com/expeditors/trackservice/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.expeditors.trackservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +16,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
    @Bean
