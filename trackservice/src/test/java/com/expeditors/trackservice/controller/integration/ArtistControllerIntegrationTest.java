b25dfa0b6d03a982759dd723e32164f3d5a1a970
Antony Alfaro <antony.alfaro@expeditors.com>
2024-06-06 09:01:13 -0600

    Finish artist controller tests

--- trackservice/src/test/java/com/expeditors/trackservice/controller/integration/ArtistControllerIntegrationTest.java
+++ trackservice/src/test/java/com/expeditors/trackservice/controller/integration/ArtistControllerIntegrationTest.java
@@ -1,25 +1,33 @@
 package com.expeditors.trackservice.controller.integration;
 
 import com.expeditors.trackservice.domain.Artist;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.Collections;
+
 import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
-public class ArtistControllerTestIntegration {
+public class ArtistControllerIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -127,4 +135,91 @@ public class ArtistControllerTestIntegration {
 
     }
 
+    @Test
+    @WithMockUser(username = "normaluser")
+    public void testAddArtistGood() throws Exception {
+
+        Artist artist = new Artist("Test", "test", Collections.emptySet());
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(artist);
+
+        ResultActions actions = mockMvc.perform(post("/artist").accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON).content(jsonString));
+
+        actions = actions.andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+        actions = actions.andExpect(status().isOk());
+
+        actions = actions.andExpect(jsonPath("$.firstName")
+                .value("Test"));
+
+        MvcResult result = actions.andReturn();
+
+        MockHttpServletResponse response = result.getResponse();
+
+        jsonString = response.getContentAsString();
+        System.out.println("resp = " + jsonString);
+    }
+
+    @Test
+    //No User here, should get 401 Unauthorized
+    public void testAddArtistUnAuthorized() throws Exception {
+        Artist artist = new Artist("Test", "test", Collections.emptySet());
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(artist);
+
+        ResultActions actions = mockMvc.perform(post("/artist").accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON).content(jsonString));
+
+        actions = actions.andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "normaluser")
+    public void testUpdateArtistGood() throws Exception {
+
+        ResultActions actions = mockMvc
+                .perform(get("/artist/by/id/1").accept(MediaType.APPLICATION_JSON));
+        String jsonString = actions.andReturn().getResponse().getContentAsString();
+        ObjectMapper mapper = new ObjectMapper();
+
+        JsonNode node = mapper.readTree(jsonString);
+
+        Artist artist = mapper.treeToValue(node, Artist.class);
+
+        artist.setLastName("test update");
+        String updatedJson = mapper.writeValueAsString(artist);
+
+        ResultActions putActions = mockMvc.perform(put("/artist/{0}", artist.getId())
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(updatedJson));
+
+        putActions = putActions.andExpect(status().isNoContent());
+
+        MvcResult result = actions.andReturn();
+
+        ResultActions postPutActions =  mockMvc
+                .perform(get("/artist/by/id/1").accept(MediaType.APPLICATION_JSON));
+        String postJson = postPutActions.andReturn().getResponse().getContentAsString();
+
+        Artist postArtist = mapper.treeToValue(mapper.readTree(postJson), Artist.class);
+        assertEquals("test update", postArtist.getLastName());
+    }
+
+    @Test
+    @WithMockUser(username = "normaluser")
+    public void testDeleteArtistGood() throws Exception {
+        ResultActions deleteActions = mockMvc.perform(delete("/artist/{0}", 2)
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON));
+
+        deleteActions = deleteActions.andExpect(status().isNoContent());
+
+        ResultActions postDeleteActions = mockMvc
+                .perform(get("/artist/by/id/2")
+                        .accept(MediaType.APPLICATION_JSON));
+
+        postDeleteActions = postDeleteActions.andExpect(status().isNotFound());
+    }
 }
