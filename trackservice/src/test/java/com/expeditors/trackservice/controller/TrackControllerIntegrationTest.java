ea8a8957e92cba3faac83d29532707ba97f30248
whynot <anil@panix.com>
2024-06-06 20:55:55 -0400

    Changes to fix ssl.

--- trackservice/src/test/java/com/expeditors/trackservice/controller/TrackControllerIntegrationTest.java
+++ trackservice/src/test/java/com/expeditors/trackservice/controller/TrackControllerIntegrationTest.java
@@ -2,12 +2,14 @@ package com.expeditors.trackservice.controller;
 
 import com.expeditors.trackservice.config.profiles.Profiles;
 
+import java.util.Base64;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.client.RestClientSsl;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestClient;
@@ -16,38 +18,44 @@ import java.util.List;
 import static com.expeditors.trackservice.config.profiles.Profiles.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-@ActiveProfiles(profiles = {JPA, PRICING_PROVIDER_LOCAL, SSL})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(profiles = {JPA, PRICING_PROVIDER_LOCAL, "ssl"})
 public class TrackControllerIntegrationTest {
 
-    @Value("${credentials}")
-    private String authHeader;
+   //    @Value("${credentials}")
+   private String authHeader;
 
-    @Autowired
-    private RestClientSsl ssl;
+   @LocalServerPort
+   private int port;
 
-    RestClient getRestClient() {
+   @Autowired
+   private RestClientSsl ssl;
 
-        return RestClient.builder()
-                .baseUrl("https://localhost:10005/track")
-                .apply(ssl.fromBundle("track-ssl"))
-                .defaultHeader("Accept", "application/json")
-                .defaultHeader("Content-Type", "application/json")
-                .defaultHeader("Authorization", authHeader)
-                .build();
-    }
+   RestClient getRestClient() {
+      authHeader = "Basic " + Base64.getEncoder()
+            .encodeToString("normaluser:everglades_user".getBytes());
+//              .encodeToString("normaluser:password".getBytes());
 
-    @Test
-    void projectAllowsSelfSignedCertificate(){
+      return RestClient.builder()
+            .baseUrl("https://localhost:" + port + "/track")
+            .apply(ssl.fromBundle("track-ssl"))
+            .defaultHeader("Accept", "application/json")
+            .defaultHeader("Content-Type", "application/json")
+            .defaultHeader("Authorization", authHeader)
+            .build();
+   }
 
-        var restClient = getRestClient();
-        var response = restClient
-                .get()
-                .retrieve()
-                .toEntity(List.class);
+   @Test
+   void projectAllowsSelfSignedCertificate() {
 
-        assertThat(response
-                .getStatusCode())
-                .isEqualTo(HttpStatus.OK);
-    }
+      var restClient = getRestClient();
+      var response = restClient
+            .get()
+            .retrieve()
+            .toEntity(List.class);
+
+      assertThat(response
+            .getStatusCode())
+            .isEqualTo(HttpStatus.OK);
+   }
 }
